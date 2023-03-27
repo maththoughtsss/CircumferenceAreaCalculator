71db18f42ae8180bbf2a9477643ad4e5276c8493
maththoughtsss <jonathan.munoz01@mayor.cl>
2023-03-27 01:13:40 -0300

    Commenting sections of code

--- Program.cs
+++ Program.cs
@@ -4,19 +4,19 @@
     {
         public static void Main(string[] args)
         {
+            //Variables
             string? strRadius;
-
             double NumRadius, circArea, pi;
             Pi = 3.14;
-
+            //Title
             Console.WriteLine("CIRCUMFERENCE AREA CALCULATOR");
-            Console.WriteLine("Enter radius: ");
-
+            //Input
+            Console.WriteLine("Enter radius: ");            
             strRadius = Console.ReadLine();
+            //Functions
             double.TryParse(strRadius, out NumRadius);
-
             circArea= NumRadius * NumRadius * pi;
-
+            //Output
             Console.WriteLine($"Total Area: {circArea} u2");
         }
     }
