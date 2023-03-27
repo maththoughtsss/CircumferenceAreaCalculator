970a19acd2e520f5828d1cab914d0750b2c0874a
maththoughtsss <jonathan.munoz01@mayor.cl>
2023-03-27 00:06:56 -0300

    A simple mathematic exercise in C#

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CircumferenceAreaCalculator
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string? strRadius;
+
+            double NumRadius, circArea, Pi;
+            Pi = 3.14;
+
+            Console.WriteLine("CIRCUMFERENCE AREA CALCULATOR");
+            Console.WriteLine("Enter radius: ");
+
+            strRadius = Console.ReadLine();
+            double.TryParse(strRadius, out NumRadius);
+
+            circArea= NumRadius * NumRadius * Pi;
+
+            Console.WriteLine($"Total Area: {circArea} u2");
+        }
+    }
+}
